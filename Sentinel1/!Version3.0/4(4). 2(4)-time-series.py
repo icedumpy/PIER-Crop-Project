22f53d05239d508902d7a633ddde2d711f92f5cb
Pongporamat Charuchinda <pongporamat.c@ku.th>
2021-03-03 10:24:26 +0700

    Dialy progress

--- Sentinel1/!Version3.0/4(4). 2(4)-time-series.py
+++ Sentinel1/!Version3.0/4(4). 2(4)-time-series.py
@@ -9,7 +9,7 @@ from icedumpy.df_tools import load_vew, clean_and_process_vew, load_mapping, set
 #%%
 root_df_vew = r"F:\CROP-PIER\CROP-WORK\vew_polygon_id_plant_date_disaster_merged"
 root_df_mapping = r"F:\CROP-PIER\CROP-WORK\Sentinel1_dataframe_updated\s1_polygon_id_rowcol_map_prov_scene_v5(at-False)"
-root_df_s1_temporal = r"F:\CROP-PIER\CROP-WORK\Sentinel1_dataframe_updated\s1_pixel_from_mapping_v5 - Backup2"
+root_df_s1_temporal = r"F:\CROP-PIER\CROP-WORK\Sentinel1_dataframe_updated\s1_pixel_from_mapping_v5"
 #%%
 strip_id = "304"
 model1 = load_model(rf"F:\CROP-PIER\CROP-WORK\Model\sentinel1\S1AB\{strip_id}-w-age\model1.joblib")
@@ -53,61 +53,6 @@ df_content = df_content.set_index("index")
 df_content.loc[df_content.index[0]-1, ["start", "stop"]] = [df_content.loc[df_content.index[0], "start"]-datetime.timedelta(days=6), df_content.loc[df_content.index[0], "start"]]
 df_content = df_content.sort_index()
 #%%
-# df_vew_batch = df_vew.loc[df_vew["loss_ratio"] > 0].sample(n=100)
-# for row in df_vew_batch.itertuples():
-#     try:
-#         pred = [0, 0]
-#         new_polygon_id = row.new_polygon_id
-#         date_plant = row.final_plant_date
-#         date_harvest = date_plant+datetime.timedelta(days=180)
-#         date_flood = row.START_DATE
-        
-#         column_plant = df_content.loc[((df_content["start"] < date_plant) & (df_content["stop"] >= date_plant))].index[0]
-#         column_harvest = df_content.loc[((df_content["start"] < date_harvest) & (df_content["stop"] >= date_harvest))].index[0]
-#         column_flood = df_content.loc[((df_content["start"] < date_flood) & (df_content["stop"] >= date_flood))].index[0]
-#         data = df_s1_temporal.loc[[new_polygon_id], df_s1_temporal.columns[column_plant:column_harvest+1]]
-#         for i in range(len(data.columns)-3):
-#             date = datetime.datetime.strptime(data.columns[i+2], "%Y%m%d")
-#             # print(date)
-#             # month = date.month
-#             age = (date - date_plant).days
-#             age = np.digitize(age, bins=[0, 40, 90], right=True)
-            
-#             model1_prob = model1.predict_proba(np.hstack([data.iloc[:, i:i+4].values, age+np.zeros((len(data), 1))]).reshape(-1, 5))[:, -1]
-#             # model1_prob = model1.predict_proba(np.hstack([data.iloc[:, i:i+3].values, age+np.zeros((len(data), 1)), month+np.zeros((len(data), 1))]).reshape(-1, 5))[:, -1]
-        
-#             model1_prob_mean = model1_prob.mean()
-#             if len(model1_prob) == 1:
-#                 model1_prob_std = 0
-#             else:
-#                 model1_prob_std = model1_prob.std()
-#             model2_bin = model2.predict(np.array([model1_prob_mean, model1_prob_std, age]).reshape(-1, 3))
-            
-#             # print("---------------------------------")
-#             # print(data.columns[i:i+3].values, str(date_flood.date()).replace("-", ""), age)
-#             # print(data.iloc[:, i:i+3].values.mean(axis=0))
-#             # print(f"Model1(mean, std): ({model1_prob_mean:.2f}, {model1_prob_std:.2f})")
-#             # print(f"Model2(Bin): {int(model2_bin)}")
-#             # print()
-            
-#             pred.append(model2_bin[0])
-#         while len(pred) < 31:
-#             pred.append(0.0)
-#         plt.close('all')
-#         fig, ax = plt.subplots()
-#         data = data.mean(axis=0)
-#         ax.plot([i for i in range(data.shape[-1])], data.values)
-#         for idx, value in enumerate(pred):
-#             if value != 0:
-#                 ax.scatter(idx, data[idx], color="red")
-#                 ax.text(idx, data[idx], f"{int(value)}")
-#         ax.axvline(column_flood - column_plant, color="red", linestyle="--")
-#         ax.grid()
-#         ax.set_title(f"{row.loss_ratio}")
-#         fig.savefig(os.path.join(r"F:\CROP-PIER\CROP-WORK\Presentation\20210208\Flood", f"{new_polygon_id}.png"))
-#     except IndexError:
-#         print(date_plant)
-#%%
 list_df = []
 for (ext_act_id, new_polygon_id, final_plant_date, final_harvest_date), df_vewl_grp in tqdm(df_vew.groupby(["ext_act_id", "new_polygon_id", "final_plant_date", "final_harvest_date"])):
     try:
@@ -150,6 +95,8 @@ for (ext_act_id, new_polygon_id, final_plant_date, final_harvest_date), df_vewl_
         data_model2 = np.vstack([data_model2.mean(axis=1), np.zeros((data_model2.shape[0], )), list_age]).T
     model2_bin = model2.predict(data_model2)
     
+    
+    
     # Append data (np.int64, np.datetime64, int)
     df = pd.DataFrame({"ext_act_id": int(ext_act_id),
                        "date":list_date,
@@ -157,7 +104,10 @@ for (ext_act_id, new_polygon_id, final_plant_date, final_harvest_date), df_vewl_
                        })
     list_df.append(df)
 #%%
-# Create result dataframe and s ave
+df
+#%%
+
+# Create result dataframe and save
 df = pd.concat(list_df, ignore_index=True)
 path_save = os.path.join(r"F:\CROP-PIER\CROP-WORK\Sentinel1_dataframe_updated", "s1_2019_prediction_p30_s304.parquet")
 df.to_parquet(path_save)
--- Sentinel1/!Version4.0/3. Features-selection-classification.py
+++ Sentinel1/!Version4.0/3. Features-selection-classification.py
@@ -86,29 +86,27 @@ root_df_s1_temporal = r"F:\CROP-PIER\CROP-WORK\Sentinel1_dataframe_updated\s1ab_
 root_df_s1_temporal_2020 = r"F:\CROP-PIER\CROP-WORK\Sentinel1_dataframe_updated\s1_pixel_from_mapping_v5_2020"
 root_df_vew_2020 = r"F:\CROP-PIER\CROP-WORK\vew_2020\vew_polygon_id_plant_date_disaster_20210202"
 root_save = r"F:\CROP-PIER\CROP-WORK\Presentation\20210224\Fig"
-strip_id = "304"
+strip_id = "402"
 #%%
 df = pd.concat([pd.read_parquet(os.path.join(root_df_s1_temporal, file)) for file in os.listdir(root_df_s1_temporal) if file.split(".")[0][-3:] == strip_id], ignore_index=True)
+df = df.drop(columns="t30") # Column "t30" is already out of season
 df = df[(df["ext_act_id"].isin(np.random.choice(df.loc[df["loss_ratio"] == 0, "ext_act_id"].unique(), len(df.loc[df["loss_ratio"] >= 0.8, "ext_act_id"].unique()), replace=False))) | (df["loss_ratio"] >= 0.8)]
-columns = df.columns[:31]
+columns = df.columns[:30]
 columns_age1 = [f"t{i}" for i in range(0, 7)]
-columns_age2 = [f"t{i}" for i in range(7, 16)]
-columns_age3 = [f"t{i}" for i in range(16, 31)]
+columns_age2 = [f"t{i}" for i in range(7, 15)]
+columns_age3 = [f"t{i}" for i in range(15, 30)]
 
 # Convert power to db
 df = convert_power_to_db(df, columns)
 
 # Assign mean, median (overall)
-df = df.assign(**{"mean":df[columns].mean(axis=1),
-                  "median":df[columns].median(axis=1),
-                  "mean(age1)":df[columns_age1].mean(axis=1),
+df = df.assign(**{"median":df[columns].median(axis=1),
                   "median(age1)":df[columns_age1].median(axis=1),
-                  "mean(age2)":df[columns_age2].mean(axis=1),
                   "median(age2)":df[columns_age2].median(axis=1),
-                  "mean(age3)":df[columns_age3].mean(axis=1),
                   "median(age3)":df[columns_age3].median(axis=1),
                   })
-df = df.assign(**{"median-min(age1)":df["median(age1)"]-df[columns_age1].min(axis=1),
+df = df.assign(**{"median-min":df["median"]-df[columns].min(axis=1),
+                  "median-min(age1)":df["median(age1)"]-df[columns_age1].min(axis=1),
                   "median-min(age2)":df["median(age2)"]-df[columns_age2].min(axis=1),
                   "median-min(age3)":df["median(age3)"]-df[columns_age3].min(axis=1),
                   })
@@ -116,44 +114,35 @@ df = df.assign(**{"median-min(age1)":df["median(age1)"]-df[columns_age1].min(axi
 # Assign sum of value under median (instead of area under median)
 df = df.assign(**{"area-under-median(age1)":(-df[columns_age1].sub(df["median(age1)"], axis=0)).clip(lower=0, upper=None).sum(axis=1),
                   "area-under-median(age2)":(-df[columns_age2].sub(df["median(age2)"], axis=0)).clip(lower=0, upper=None).sum(axis=1),
-                  "area-under-median(age3)":(-df[columns_age3].sub(df["median(age3)"], axis=0)).clip(lower=0, upper=None).sum(axis=1)})
+                  "area-under-median(age3)":(-df[columns_age3].sub(df["median(age3)"], axis=0)).clip(lower=0, upper=None).sum(axis=1)
+                  })
 
-
+df = df.assign(**{"min(age1)":df[columns_age1].min(axis=1),
+                  "min(age2)":df[columns_age2].min(axis=1),
+                  "min(age3)":df[columns_age3].min(axis=1),
+                  })
 
 df_nonflood = df[df["label"] == 0]
 df_flood = df[df["label"] == 1]
 
 # Add flood column
-df_flood = df_flood.assign(flood_column = (df_flood["START_DATE"]-df_flood["final_plant_date"]).dt.days//6)
+# df_flood = df_flood.assign(flood_column = (df_flood["START_DATE"]-df_flood["final_plant_date"]).dt.days//6)
 #%%
-df_sample = df.sample(n=1).squeeze()
+plt.close('all')
+sns.histplot(df_flood.iloc[:, -6:-3].max(axis=1), label="flood", bins="auto", color="blue", kde=True)
+sns.histplot(df_nonflood.iloc[:, -6:-3].max(axis=1), label="nonflood", bins="auto", color="salmon", kde=True)
+plt.legend()
 
-plt.close("all")
-fig, ax = initialize_plot(ylim=(-20, -5))
-
-# Plot temporal
-ax.plot(df_sample[columns].values)
-
-# Plot mean, median (age1)
-ax.hlines(df_sample["mean(age1)"], xmin=0, xmax=6.5, linestyle="-.", color="green", label="Mean (Age1)")
-ax.hlines(df_sample["median(age1)"], xmin=0, xmax=6.5, linestyle="--", color="orange", label="Median (Age1)")
-
-# Plot mean, median (age2)
-ax.hlines(df_sample["mean(age2)"], xmin=6.5, xmax=15.5, linestyle="-.", color="black", label="Mean (age2)")
-ax.hlines(df_sample["median(age2)"], xmin=6.5, xmax=15.5, linestyle="--", color="gray", label="Median (age2)")
-
-# Plot mean, median (age3)
-ax.hlines(df_sample["mean(age3)"], xmin=15.5, xmax=30, linestyle="-.", color="red", label="Mean (age3)")
-ax.hlines(df_sample["median(age3)"], xmin=15.5, xmax=30, linestyle="--", color="blue", label="Median (age3)")
-
-# Add final details
-ax.legend()
-ax.grid(linestyle="--")
-ax.set_xlabel(df_sample["label"])
+plt.figure()
+sns.histplot(df_flood.iloc[:, -3:].max(axis=1), label="flood", bins="auto", color="blue", kde=True)
+sns.histplot(df_nonflood.iloc[:, -3:].max(axis=1), label="nonflood", bins="auto", color="salmon", kde=True)
+plt.legend()
 #%%
-x_train = df.loc[~(df["ext_act_id"]%10).isin([8, 9]), ["median-min(age1)", "median-min(age2)", "median-min(age3)", "area-under-median(age1)", "area-under-median(age2)", "area-under-median(age3)"]].values
+# x_train = df.loc[~(df["ext_act_id"]%10).isin([8, 9]), ["median-min(age1)", "median-min(age2)", "median-min(age3)", "area-under-median(age1)", "area-under-median(age2)", "area-under-median(age3)"]].values
+x_train = df.loc[~(df["ext_act_id"]%10).isin([8, 9]), df.columns[-14:]].values
 y_train = df.loc[~(df["ext_act_id"]%10).isin([8, 9]), "label"].values
-x_test = df.loc[(df["ext_act_id"]%10).isin([8, 9]), ["median-min(age1)", "median-min(age2)", "median-min(age3)", "area-under-median(age1)", "area-under-median(age2)", "area-under-median(age3)"]].values
+# x_test = df.loc[(df["ext_act_id"]%10).isin([8, 9]), ["median-min(age1)", "median-min(age2)", "median-min(age3)", "area-under-median(age1)", "area-under-median(age2)", "area-under-median(age3)"]].values
+x_test = df.loc[(df["ext_act_id"]%10).isin([8, 9]),  df.columns[-14:]].values
 y_test = df.loc[(df["ext_act_id"]%10).isin([8, 9]), "label"].values
 #%%
 # model = RandomForestClassifier(n_jobs=-1)
@@ -161,7 +150,7 @@ model = RandomForestClassifier(min_samples_leaf=5, max_depth=10, min_samples_spl
                                verbose=1, n_jobs=-1, random_state=42)
 model.fit(x_train, y_train)
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(16, 9))
 plot_roc_curve(model, x_train, y_train, label="train", color="b-", ax=ax)
 plot_roc_curve(model, x_test, y_test, label="test", color="g-", ax=ax)
 ax = set_roc_plot_template(ax)
