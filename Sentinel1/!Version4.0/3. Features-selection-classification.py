378745eb0c4fa0511e29bd9357d055ba0c6c0626
Pongporamat Charuchinda <pongporamat.c@ku.th>
2021-03-15 12:41:54 +0700

    Daily progress

--- Sentinel1/!Version4.0/3. Features-selection-classification.py
+++ Sentinel1/!Version4.0/3. Features-selection-classification.py
@@ -15,12 +15,12 @@ def interp_numba(arr_ndvi):
     '''
     Interpolate an array in both directions using numba.
     (From P'Tee+)
-
+    
     Parameters
     ---------
     arr_ndvi : numpy.array(np.float32)
         An array of NDVI values to be interpolated.
-
+    
     Returns
     ---------
     arr_ndvi : numpy.array(np.float32)
@@ -34,26 +34,26 @@ def interp_numba(arr_ndvi):
         if len(arr_ndvi_row_not_nan_idx) > 0:
             arr_ndvi_row_not_nan_values = arr_ndvi_row[arr_ndvi_row_not_nan_idx]
             arr_ndvi[n_row] = np.interp(arr_ndvi_row_idx, arr_ndvi_row_not_nan_idx, arr_ndvi_row_not_nan_values)
-
+    
     arr_ndvi = arr_ndvi.astype(np.float32)
     return arr_ndvi
 
 def convert_power_to_db(df, columns):
     df = df.copy()
-
+    
     # Replace negatives with nan
     for col in columns:
         df.loc[df[col] <= 0, col] = np.nan
-
+    
     # Drop row with mostly nan
     df = df.loc[df[columns].isna().sum(axis=1) < 10]
-
+    
     # Interpolate nan values
     df[columns] = interp_numba(df[columns].values)
-
+    
     # Convert power to dB
     df[columns] = 10*np.log10(df[columns])
-
+    
     # Assign label
     df.loc[df["loss_ratio"] == 0, "label"] = 0
     df.loc[df["loss_ratio"] >= 0.8, "label"] = 1
@@ -62,7 +62,7 @@ def convert_power_to_db(df, columns):
 def initialize_plot(ylim=(-20, -5)):
     # Initialize figure
     fig, ax = plt.subplots(figsize=(16, 9))
-
+    
     # Draw group age
     ax.axvspan(0.0, 6.5, alpha=0.2, color='red')
     ax.axvspan(6.5, 15.5, alpha=0.2, color='green')
@@ -73,19 +73,21 @@ def initialize_plot(ylim=(-20, -5)):
     ax.text(2.5, ylim[-1]+0.25, "0-40 days")
     ax.text(10, ylim[-1]+0.25, "40-90 days")
     ax.text(22, ylim[-1]+0.25, "90+ days")
-
+    
     # Set y limits
     ax.set_ylim(ylim)
-
+    
     # Add more ticks
     ax.set_xticks(range(31))
     ax.set_yticks(np.arange(*ylim))
-
+    
     return fig, ax
 
 @jit(nopython=True)
 def get_area_under_median(arr_min_index, arr_value_under_median):
     arr_area_under_median = np.zeros_like(arr_min_index, dtype=np.float32)
+    arr_is_consecutive = np.ones_like(arr_min_index, dtype=np.uint8) # 0:Consecutive first, 1:No consecutive, 2:Consecutive last
+    arr_consecutive_size = np.zeros_like(arr_min_index, dtype=np.uint8)
     for i, (index_min, value_under_median)in enumerate(zip(arr_min_index, arr_value_under_median)):
         arr_group = np.zeros((arr_value_under_median.shape[-1]))
         group = 1
@@ -94,9 +96,16 @@ def get_area_under_median(arr_min_index, arr_value_under_median):
                 arr_group[j] = group
             else:
                 group+=1
-        area_under_median = value_under_median[np.where(arr_group == arr_group[index_min])[0]].sum()
+        
+        arr_where_min_group = np.where(arr_group == arr_group[index_min])[0]
+        area_under_median = value_under_median[arr_where_min_group].sum()
         arr_area_under_median[i] = area_under_median
-    return arr_area_under_median
+        if arr_where_min_group[0] == 0:
+            arr_is_consecutive[i] = 0
+        elif arr_where_min_group[-1] == arr_value_under_median.shape[-1]-1:
+            arr_is_consecutive[i] = 2
+        arr_consecutive_size[i] = len(arr_where_min_group)
+    return arr_area_under_median, arr_is_consecutive, arr_consecutive_size
 #%%
 root_df_s1_temporal = r"F:\CROP-PIER\CROP-WORK\Sentinel1_dataframe_updated\s1ab_temporal"
 root_df_s1_temporal_2020 = r"F:\CROP-PIER\CROP-WORK\Sentinel1_dataframe_updated\s1_pixel_from_mapping_v5_2020"
@@ -129,7 +138,7 @@ df = convert_power_to_db(df, columns)
 
 # Drop duplicates
 df = df.drop_duplicates(subset=columns)
-#%%
+
 # Assign median
 df = df.assign(**{"median":df[columns].median(axis=1),
                   "median(age1)":df[columns_age1].median(axis=1),
@@ -144,13 +153,6 @@ df = df.assign(**{"median-min":df["median"]-df[columns].min(axis=1),
                   "median-min(age2)":df["median(age2)"]-df[columns_age2].min(axis=1),
                   "median-min(age3)":df["median(age3)"]-df[columns_age3].min(axis=1),
                   "median-min(age4)":df["median(age4)"]-df[columns_age4].min(axis=1),
-                  })
-
-# Assign sum of cunsecutive values under median (around min value)
-df = df.assign(**{"area-under-median(age1)" : get_area_under_median(np.argmax(df[columns_age1].eq(df[columns_age1].min(axis=1), axis=0).values, axis=1), (-df[columns_age1].sub(df["median(age1)"], axis=0)).clip(lower=0, upper=None).values),
-                  "area-under-median(age2)" : get_area_under_median(np.argmax(df[columns_age2].eq(df[columns_age2].min(axis=1), axis=0).values, axis=1), (-df[columns_age2].sub(df["median(age2)"], axis=0)).clip(lower=0, upper=None).values),
-                  "area-under-median(age3)" : get_area_under_median(np.argmax(df[columns_age3].eq(df[columns_age3].min(axis=1), axis=0).values, axis=1), (-df[columns_age3].sub(df["median(age3)"], axis=0)).clip(lower=0, upper=None).values),
-                  "area-under-median(age4)" : get_area_under_median(np.argmax(df[columns_age4].eq(df[columns_age4].min(axis=1), axis=0).values, axis=1), (-df[columns_age4].sub(df["median(age4)"], axis=0)).clip(lower=0, upper=None).values)
                   })
 
 # Assign min value 
@@ -168,6 +170,49 @@ df = df.assign(**{"max-min":df[columns].max(axis=1)-df[columns].min(axis=1),
                   "max-min(age4)":df[columns_age4].max(axis=1)-df[columns_age4].min(axis=1),
                   })
 
+# =============================================================================
+# Assign area under median, consecutive under median
+# =============================================================================
+arr_area_under_median_age1, arr_is_consecutive_age1, arr_consecutive_size_age1 = get_area_under_median(np.argmax(df[columns_age1].eq(df[columns_age1].min(axis=1), axis=0).values, axis=1), (-df[columns_age1].sub(df["median(age1)"], axis=0)).clip(lower=0, upper=None).values)
+arr_area_under_median_age2, arr_is_consecutive_age2, arr_consecutive_size_age2 = get_area_under_median(np.argmax(df[columns_age2].eq(df[columns_age2].min(axis=1), axis=0).values, axis=1), (-df[columns_age2].sub(df["median(age2)"], axis=0)).clip(lower=0, upper=None).values)
+arr_area_under_median_age3, arr_is_consecutive_age3, arr_consecutive_size_age3 = get_area_under_median(np.argmax(df[columns_age3].eq(df[columns_age3].min(axis=1), axis=0).values, axis=1), (-df[columns_age3].sub(df["median(age3)"], axis=0)).clip(lower=0, upper=None).values)
+arr_area_under_median_age4, arr_is_consecutive_age4, arr_consecutive_size_age4 = get_area_under_median(np.argmax(df[columns_age4].eq(df[columns_age4].min(axis=1), axis=0).values, axis=1), (-df[columns_age4].sub(df["median(age4)"], axis=0)).clip(lower=0, upper=None).values)
+
+# 1 <-> 2, 2 <-> 1
+arr_index = np.where((arr_is_consecutive_age1 == 2) & (arr_is_consecutive_age2 == 0))[0]
+arr_consecutive_size_age1[arr_index] += arr_consecutive_size_age2[arr_index]
+arr_consecutive_size_age2[arr_index] += arr_consecutive_size_age1[arr_index]
+arr_area_under_median_age1[arr_index] += arr_area_under_median_age2[arr_index]
+arr_area_under_median_age2[arr_index] += arr_area_under_median_age1[arr_index]
+
+# 2 <-> 3, 3 <-> 2
+arr_index = np.where((arr_is_consecutive_age2 == 2) & (arr_is_consecutive_age3 == 0))[0]
+arr_consecutive_size_age2[arr_index] += arr_consecutive_size_age3[arr_index]
+arr_consecutive_size_age3[arr_index] += arr_consecutive_size_age2[arr_index]
+arr_area_under_median_age2[arr_index] += arr_area_under_median_age3[arr_index]
+arr_area_under_median_age3[arr_index] += arr_area_under_median_age2[arr_index]
+
+# 3 <-> 4, 4 <-> 3
+arr_index = np.where((arr_is_consecutive_age3 == 2) & (arr_is_consecutive_age4 == 0))[0]
+arr_consecutive_size_age3[arr_index] += arr_consecutive_size_age4[arr_index]
+arr_consecutive_size_age4[arr_index] += arr_consecutive_size_age3[arr_index]
+arr_area_under_median_age3[arr_index] += arr_area_under_median_age4[arr_index]
+arr_area_under_median_age4[arr_index] += arr_area_under_median_age3[arr_index]
+
+# Assign sum of cunsecutive values under median (around min value)
+df = df.assign(**{"area-under-median(age1)" : arr_area_under_median_age1,
+                  "area-under-median(age2)" : arr_area_under_median_age2,
+                  "area-under-median(age3)" : arr_area_under_median_age3,
+                  "area-under-median(age4)" : arr_area_under_median_age4
+                  })
+
+# Assign count of cunsecutive values under median (around min value)
+df = df.assign(**{"count-under-median(age1)" : arr_consecutive_size_age1,
+                  "count-under-median(age2)" : arr_consecutive_size_age2,
+                  "count-under-median(age3)" : arr_consecutive_size_age3,
+                  "count-under-median(age4)" : arr_consecutive_size_age4
+                  })
+
 # Merge photo sensitivity
 df = pd.merge(df, df_rice_code, on="BREED_CODE", how="inner")
 
@@ -177,12 +222,9 @@ df_flood = df[df["label"] == 1]
 # Add flood column
 # df_flood = df_flood.assign(flood_column = (df_flood["START_DATE"]-df_flood["final_plant_date"]).dt.days//6)
 #%%
-# plt.close('all')
-# sns.histplot(data=df, x="median", hue="label")
-#%%
-x_train = df.loc[~(df["ext_act_id"]%10).isin([8, 9]), df.columns[-24:]].values
+x_train = df.loc[~(df["ext_act_id"]%10).isin([8, 9]), df.columns[-28:]].values
 y_train = df.loc[~(df["ext_act_id"]%10).isin([8, 9]), "label"].values
-x_test = df.loc[(df["ext_act_id"]%10).isin([8, 9]),  df.columns[-24:]].values
+x_test = df.loc[(df["ext_act_id"]%10).isin([8, 9]),  df.columns[-28:]].values
 y_test = df.loc[(df["ext_act_id"]%10).isin([8, 9]), "label"].values
 #%%
 # model = RandomForestClassifier(n_jobs=-1)
@@ -196,7 +238,7 @@ plot_roc_curve(model, x_train, y_train, label="train", color="b-", ax=ax)
 plot_roc_curve(model, x_test, y_test, label="test", color="g-", ax=ax)
 ax = set_roc_plot_template(ax)
 ax.set_title(f'ROC Curve: {strip_id}\nAll_touched(False), Tier{(1,)}\nTrain samples: Flood:{np.bincount(y_train.astype(int))[1]:,}, Non-Flood:{np.bincount(y_train.astype(int))[0]:,}\nTest samples: Flood:{np.bincount(y_test.astype(int))[1]:,}, Non-Flood:{np.bincount(y_test.astype(int))[0]:,}')
-# fig.savefig(rf"F:\CROP-PIER\CROP-WORK\Presentation\20210309\Fig\{strip_id}.png")
+# fig.savefig(rf"F:\CROP-PIER\CROP-WORK\Presentation\20210312\Fig\{strip_id}.png")
 #%%
 df_pred = pd.DataFrame(df.loc[(df["ext_act_id"]%10).isin([8, 9])])
 df_pred = df_pred.drop(columns="label")
