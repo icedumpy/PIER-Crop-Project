bf86a7345556c63f17cfc180e66f3db4cfc933a8
Pongporamat Charuchinda <pongporamat.c@ku.th>
2022-01-25 14:59:47 +0700

    Update

--- Sentinel1/!Version7.0/!Error-viz/1. Visualization.py
+++ Sentinel1/!Version7.0/!Error-viz/1. Visualization.py
@@ -12,15 +12,14 @@ list_p = list(map(str, [30, 31, 32, 33, 34, 35, 36, 37, 38, 40, 41, 44, 45, 46, 
 list_df = []
 for file in tqdm([file for file in os.listdir(root_vew) if (file.split(".")[0].split("_")[-2][1:] in list_p) and (int(file.split(".")[0].split("_")[-1]) >= 2017)]):
     df = pd.read_parquet(os.path.join(root_vew, file))
-    list_df.append(df[["ext_act_id", "polygon"]])
+    list_df.append(df[["ext_act_id", "final_plant_year", "polygon"]])
 df = pd.concat(list_df, ignore_index=True)
+df = df.rename(columns={"final_plant_year":"plant_year"})
 gdf = convert_to_geodataframe(df, polygon_column="polygon")
+del list_df
 #%% Loop for each result file
 for file in os.listdir(root_p_tee):
     print(file)
-    path_save = os.path.join(root_save, file.replace(".parquet", ".shp"))
-    if os.path.exists(path_save):
-        continue
     
     path = os.path.join(root_p_tee, file)
     df = pd.read_parquet(path)
@@ -60,15 +59,15 @@ for file in os.listdir(root_p_tee):
     df.loc[df["resul_type"] == 8, "[Pred Act]"] = "8:[0 1]"
     df.loc[df["resul_type"] == 9, "[Pred Act]"] = "9:[1 2]"
     
-    # Save gdf
-    df.to_file(path_save)
+    # Groupby year and save
+    for year, df_grp in df.groupby("plant_year"):
+        path_save = os.path.join(root_save, str(year), file.replace(".parquet", f"_{year}.shp"))
+        os.makedirs(os.path.dirname(path_save), exist_ok=True)
+        df_grp.to_file(path_save)
 #%%
 root_tai = r"F:\CROP-PIER\CROP-WORK\20211207-PIERxDA-batch_3c-NE3\Error\100k test"
 for file in os.listdir(root_tai):
     print(file)
-    path_save = os.path.join(root_save, file.replace(".csv", ".shp"))
-    if os.path.exists(path_save):
-        continue
     
     path = os.path.join(root_tai, file)
     df_tai = pd.read_csv(path).iloc[:, 1:]
@@ -111,23 +110,8 @@ for file in os.listdir(root_tai):
     df_tai.loc[df_tai["resul_type"] == 8, "[Pred Act]"] = "8:[0 1]"
     df_tai.loc[df_tai["resul_type"] == 9, "[Pred Act]"] = "9:[1 2]"
     
-    # Save gdf
-    df_tai.to_file(path_save)
-    break
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # Groupby year and save
+    for year, df_grp in df.groupby("plant_year"):
+        path_save = os.path.join(root_save, str(year), file.replace(".csv", f"_{year}.shp"))
+        os.makedirs(os.path.dirname(path_save), exist_ok=True)
+        df_grp.to_file(path_save)
